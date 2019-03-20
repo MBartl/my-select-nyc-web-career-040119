002a78bca78ba510aac6fb7774618fb53653a97a
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 18:09:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  new_array = []
  while i < collection.length do
-   if yield collection[i] == true
+   new_array.push(yield collection[i])
    i += 1
  end
  new_array
