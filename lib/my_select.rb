badacae21d7343dcc6d09320334413e1984e2f94
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 18:03:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,11 @@
 def my_select(collection)
  i = 0
+ new_array = []
  while i < collection.length do
    if collection[i] == true
-     yield collection[i]
+     new_array.push(collection[i])
    end
    i += 1
  end
- collection
+ new_array
 end
